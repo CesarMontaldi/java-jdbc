64b5d775e7bf1c419a658d13e5dee61d882fa7db
Cesar <guto_montaldi@yahoo.com.br>
2024-01-22 16:10:48 -0300

    Criando INSERT ao banco de dados.

--- src/main/java/br/com/cesarmontaldi/DAO/UserDAO.java
+++ src/main/java/br/com/cesarmontaldi/DAO/UserDAO.java
@@ -1,8 +1,11 @@
 package br.com.cesarmontaldi.DAO;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import br.com.cesarmontaldi.conexaojdbc.SingleConnection;
+import br.com.cesarmontaldi.model.User;
 
 public class UserDAO {
 	
@@ -10,6 +13,27 @@ public class UserDAO {
 	
 	public UserDAO() {
 		connection = SingleConnection.getConnection();
+	}
+	
+	public void salvar(User user) {
+		try {
+			String sql = "insert into users (id, nome, email) values (?, ?, ?)";
+			PreparedStatement insert = connection.prepareStatement(sql);
+			insert.setLong(1, user.getId());
+			insert.setString(2, user.getNome());
+			insert.setString(3, user.getEmail());
+			insert.execute();
+			connection.commit();// salva no banco de dados.
+		
+		}catch (Exception e) {
+			try {
+				connection.rollback();// reverte a operação caso aconteça algum erro.
+			} catch (SQLException c) {
+				// TODO Auto-generated catch block
+				c.printStackTrace();
+			}
+			e.printStackTrace();
+		}
 	}
 
 }
--- src/test/java/br/com/cesarmontaldi/java_jdbc/TesteBancoJdbc.java
+++ src/test/java/br/com/cesarmontaldi/java_jdbc/TesteBancoJdbc.java
@@ -2,13 +2,20 @@ package br.com.cesarmontaldi.java_jdbc;
 
 import org.junit.Test;
 
-import br.com.cesarmontaldi.conexaojdbc.SingleConnection;
+import br.com.cesarmontaldi.DAO.UserDAO;
+import br.com.cesarmontaldi.model.User;
 
 public class TesteBancoJdbc {
 	
 	@Test
 	public void initBanco() {
-		SingleConnection.getConnection();
+		UserDAO userDAO = new UserDAO();
+		User user = new User();
+		
+		user.setId(5L);
+		user.setNome("Carlos Silva");
+		user.setEmail("carlos.silva@yahoo.com.br");
+		userDAO.salvar(user);
 	}
 
 }
